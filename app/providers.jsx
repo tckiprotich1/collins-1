9b895151632235280019e9e96bcca53ee7da2ebb
tckiprotich1 <132175061+tckiprotich1@users.noreply.github.com>
2023-05-01 21:14:58 +0000

    default darkmode

--- app/providers.jsx
+++ app/providers.jsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+
 
 export function Providers({ children }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="light">
+    <ThemeProvider attribute="class" defaultTheme="dark">
       {children}
     </ThemeProvider>
   );
